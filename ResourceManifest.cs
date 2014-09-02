70345f32a4a1b0a8f5247dc0bac6387e5c93beb3
bp <bp@ivnet.co.uk>
2014-09-02 14:16:16 +0100

    bootstrap setup refactor

---
+++ ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.UI.Resources;
+
+namespace Themes.VanityFayreHairDesign.Theme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineScript("Bootstrap").SetUrl("Bootstrap/bootstrap.min.js").SetVersion("3.1.1");
+            manifest.DefineStyle("Bootstrap").SetUrl("Bootstrap/bootstrap.min.css").SetVersion("3.1.1");
+        }
+    }
+}
