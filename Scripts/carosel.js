31ab13592445cc70e458e8c184319fd56a371ea8
bp <bp@ivnet.co.uk>
2014-09-02 10:20:07 +0100

    Setup Repositories

---
+++ Scripts/carosel.js
@@ -0,0 +1,22 @@
+﻿var ivNet;
+(function (ivNet) {
+
+    (function (carosel) {
+       
+        carosel.initialise = initialise;
+
+        // api
+        // root
+        function initialise() {
+            setup();
+            //eventBinding();
+        }
+
+        function setup() {
+           
+
+        }
+    })(ivNet.carosel || (ivNet.carosel = {}));
+
+    var carosel = ivNet.carosel;
+})(ivNet || (ivNet = {}));
